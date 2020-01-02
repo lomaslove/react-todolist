666f9d334eb2e3758dc2f2e1099c8506065b4b17
rails course from udemy <lomaslove132@gmail.com>
2020-01-02 21:35:14 +1000

    todo list

---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Item from './TodoItem';
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                <h2> to do list</h2>
+                <Item />
+            </div>
+            
+            
+        )
+    }
+}
