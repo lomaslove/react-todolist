c8c35aa674f93941de70d79293913dc71d107823
rails course from udemy <lomaslove132@gmail.com>
2020-01-03 19:56:31 +1000

    app component added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,58 @@ import TodoList from './components/TodoList';
 import TodoItem from './components/TodoItem';
 
 class App extends Component{
+  state = {
+    items: [{ id:1, title: "wake-up"},
+            { id:2, title: "breakfast" }],
+            id: uuid(),
+            item: '',
+            editItem: false
+           };
+
+    handleChange= (e)=> {
+      console.log("handle change");
+    };
+
+    handleSubmit = e =>{
+      console.log("handle Submit")
+    };
+
+    clearList = e=>{
+      console.log("clear list")
+    };
+
+    handleDelete = id=>{
+      console.log(`handle delete ${id}`);
+    };
+
+    handleEdit = id => {
+      console.log(`edit edit ${id}`)
+    };
+
   render() {
   return (
-      <div>
+    
     <div className="container">
         <div className="row">
-                <TodoInput />
-                <TodoList />
-            </div>
-        </div>
-    </div>
+         <div className=" col-10.mx-auto.col-md-8.mt-5">
+           <h3 className="text-capatilize text-center">
+             to do input
+           </h3>
+           <TodoInput item={this.state.item}
+           handleChange={this.handleChange}
+           handleSubmit={this.handleSubmit}
+           editItem={this.state.editItem}
+           />
+           <TodoList items={this.state.items}
+           clearList={this.clearList}
+           handleDelete={this.handleDelete}
+           handleEdit={this.handleEdit}
+          />
+
+         </div>
+
+         </div>
+         </div>       
   );
 }
 }
